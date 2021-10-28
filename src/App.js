40590582b0fb401bced92970ea29d938a85b4382
Micaela Consens <micaela.consens@sickkids.ca>
2021-10-28 16:30:14 -0400

    react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
+import 'semantic-ui-css/semantic.min.css'
+import { Grid, Button, Container, Label, Modal, Form, Segment, Header, Input, Image, Icon, Divider} from 'semantic-ui-react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <style>
+      {`
+      html, body {
+        background-color: #252839 !important;
+      }
+      p {
+        align-content: center;
+        background-color: #495285;
+        color: #fff;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        min-height: 6em;
+      }
+      p > span {
+        opacity: 0.4;
+        text-align: center;
+      }
+    }
+    `}
+    </style>
+
+      <Header as='h2' inverted icon textAlign='center'>
+        <Icon medium color="olive" name='dollar sign' />
+        Price Checkout Calculator
+        <Header.Subheader>
+          Add some items to your cart to calculate their price.
+        </Header.Subheader>
+      </Header>
+    <Divider />     
+    <Grid columns={3} divided>
+      <Grid.Row>
+      </Grid.Row>
+
+      <Grid.Row>
+      </Grid.Row>
+    </Grid>
+    <Divider />
+  </Container>
   );
 }
+
+
 
 export default App;
